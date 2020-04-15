a721bc92e0b56ae13a9a0f24e51b6d44ad54d8a8
Morgan Matney <morgan.matney89@gmail.com>
2020-04-15 01:01:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, name) {
-  return `Welcome, ${name}. You are number ${line} in line.`;
-}
+function takeANumber(line, name) {
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
 
-function nowServing(katzDeliLine) {
+function nowServing(line) {
   return line[0];
   if (katzDeliLine > 0) {
     katzDeliLine.slice(1)
   } else {
    return "There is nobody waiting to be served!";
-  }
+ }
 }
 
-function currentLine(katzDeliLine) {
+function currentLine(line) {
   while
 }
